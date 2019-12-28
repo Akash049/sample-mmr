79a3386ca47a80fec3b1a7562a0580e648561a04
MelpApp ML <melpml@gmail.com>
2019-12-28 08:54:48 +0530

    build

---
+++ precache-manifest.13937a64c14afbd963ff72c219993c0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "765d4997a1aad8b8d897",
+    "url": "/static/css/main.9372e59a.chunk.css"
+  },
+  {
+    "revision": "765d4997a1aad8b8d897",
+    "url": "/static/js/main.5b8a6fb1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "64e53943a29cfece5f8e",
+    "url": "/static/css/2.db2fa2fc.chunk.css"
+  },
+  {
+    "revision": "64e53943a29cfece5f8e",
+    "url": "/static/js/2.f173177c.chunk.js"
+  },
+  {
+    "revision": "791ed88436c2f9f413c2e9d0a676fab9",
+    "url": "/index.html"
+  }
+];
